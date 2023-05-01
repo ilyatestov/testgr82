88d317ea91b7464b39682287f6cfe26bc50991c5
ilyatestov <132242847+ilyatestov@users.noreply.github.com>
2023-05-01 11:21:03 +0300

    Update shortlinks.php

--- script/shortlinks.php
+++ script/shortlinks.php
@@ -7,21 +7,9 @@
 $settings['sldata']['10000'] = array(
     'id' => '10000', // Start with id greater than 10000
     'name' => 'Example1', // Name of Shortlink
-    'apilink' => 'https://example.com/api?api={apikey}&url={url}', // leave ?api={apikey}&url={url} just change url!
+    'apilink' => 'https://usalink.io/api?api={apikey}&url=https://shorti.io/api?api={apikey}&url={url}', // leave ?api={apikey}&url={url} just change url!
     'views' => '1', // Max view count of shortener
     'cpm' => '11.00', // CPM of Shortener
     'referral' => 'https://example.com', // Your Referral link
     'status' => 'N' // Should be Y unless you dont want it to show in list then put N
 );
-
-$settings['sldata']['10001'] = array(
-    'id' => '10001', // Start with id greater than 10000
-    'name' => 'Example2', // Name of Shortlink
-    'apilink' => 'https://example.com/api?api={apikey}&url={url}', // leave ?api={apikey}&url={url} just change url!
-    'views' => '1', // Max view count of shortener
-    'cpm' => '11.00', // CPM of Shortener
-    'referral' => 'https://example.com', // Your Referral link
-    'status' => 'N' // Should be Y unless you dont want it to show in list then put N
-);
-
-
