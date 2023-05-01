f6b42b3cacb3fa5669a4c8d53485e243391ead4c
ilyatestov <132242847+ilyatestov@users.noreply.github.com>
2023-05-01 08:44:12 +0300

    Delete functions.php

--- admin/functions.php
+++
@@ -1,7 +0,0 @@
-function add_custom_shortlink($name, $url) {
-    global $pdo;
-    $stmt = $pdo->prepare("INSERT INTO shortlinks (name, url) VALUES (:name, :url)");
-    $stmt->bindParam(':name', $name);
-    $stmt->bindParam(':url', $url);
-    return $stmt->execute();
-}
