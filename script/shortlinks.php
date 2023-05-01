6785fd329d6fcd4ab85c444f4f4793237c40c39e
ilyatestov <132242847+ilyatestov@users.noreply.github.com>
2023-05-01 07:52:28 +0300

    Update shortlinks.php

--- script/shortlinks.php
+++ script/shortlinks.php
@@ -13,3 +13,13 @@ $settings['sldata']['10000'] = array(
     'referral' => 'https://example.com', // Your Referral link
     'status' => 'N' // Should be Y unless you dont want it to show in list then put N
 );
+
+$settings['sldata']['10001'] = array(
+    'id' => '10001', // Start with id greater than 10000
+    'name' => 'Example2', // Name of Shortlink
+    'apilink' => 'https://example.com/api?api={apikey}&url={url}', // leave ?api={apikey}&url={url} just change url!
+    'views' => '1', // Max view count of shortener
+    'cpm' => '11.00', // CPM of Shortener
+    'referral' => 'https://example.com', // Your Referral link
+    'status' => 'N' // Should be Y unless you dont want it to show in list then put N
+);
