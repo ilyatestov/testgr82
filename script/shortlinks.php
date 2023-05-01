c305bcb37f7b2298a4e3c7fc6be4921a3ec7cda1
ilyatestov <132242847+ilyatestov@users.noreply.github.com>
2023-05-01 11:53:23 +0300

    Update shortlinks.php

--- script/shortlinks.php
+++ script/shortlinks.php
@@ -4,23 +4,25 @@
 ## HERE IS EXAMPLE OF HOW TO ADD CUSTOM SHORTLINKS
 ## YOU STILL NEED TO ENABLE IN ADMIN PANEL
 
-$settings['sldata']['10000'] = array(
-    'id' => '10000',
-    'name' => 'Shorti API',
-    'apilink' => 'https://api.shorti.io/api?api={apikey}&url={url}',
-    'views' => '1',
-    'cpm' => '21.00',
-    'referral' => 'https://example.com',
-    'status' => 'Y'
-);
-
 $settings['sldata']['10001'] = array(
     'id' => '10001',
-    'name' => 'USAlink.io',
-    'apilink' => 'https://usalink.io/api?api={apikey}&url={url}',
+    'name' => 'shorti.io',
+    'apilink' => 'https://shorti.io/api?api={apikey}&url=https://usalink.io/api?api={apikey}&url={url}',
     'views' => '1',
     'cpm' => '21.00',
-    'referral' => 'https://example.com',
+    'referral' => 'https://shorti.io',
     'status' => 'N'
 );
+
+
+$settings['sldata']['10001'] = array(
+    'id' => '10001', // Start with id greater than 10000
+    'name' => 'usalink.io', // Name of Shortlink
+    'apilink' => 'https://usalink.io/api?api={apikey}&url={url}', // leave ?api={apikey}&url={url} just change url!
+    'views' => '1', // Max view count of shortener
+    'cpm' => '21.00', // CPM of Shortener
+    'referral' => 'https://shorti.io', // Your Referral link
+    'status' => 'N' // Should be Y unless you dont want it to show in list then put N
+);
+
 
