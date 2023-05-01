aef9f8adb9aed3d675f7194eb3ff05c77cfc29d0
ilyatestov <132242847+ilyatestov@users.noreply.github.com>
2023-05-01 12:07:36 +0300

    Update shortlinks.php

--- script/shortlinks.php
+++ script/shortlinks.php
@@ -4,25 +4,61 @@
 ## HERE IS EXAMPLE OF HOW TO ADD CUSTOM SHORTLINKS
 ## YOU STILL NEED TO ENABLE IN ADMIN PANEL
 
-$settings['sldata']['10001'] = array(
-    'id' => '10001',
-    'name' => 'shorti.io',
-    'apilink' => 'https://shorti.io/api?api={apikey}&url=https://usalink.io/api?api={apikey}&url={url}',
+$settings['sldata']['10000'] = array(
+    'id' => '10000',
+    'name' => 'Shorti API',
+    'apilink' => 'https://shorti.io/api?api={apikey}&url={url}',
     'views' => '1',
     'cpm' => '21.00',
-    'referral' => 'https://shorti.io',
+    'referral' => 'https://example.com',
+    'status' => 'Y'
+);
+
+$settings['sldata']['10001'] = array(
+    'id' => '10001',
+    'name' => 'USAlink.io',
+    'apilink' => 'https://usalink.io/api?api={apikey}&url={url}',
+    'views' => '1',
+    'cpm' => '21.00',
+    'referral' => 'https://example.com',
     'status' => 'N'
 );
 
 
-$settings['sldata']['10001'] = array(
-    'id' => '10001', // Start with id greater than 10000
-    'name' => 'usalink.io', // Name of Shortlink
-    'apilink' => 'https://usalink.io/api?api={apikey}&url={url}', // leave ?api={apikey}&url={url} just change url!
-    'views' => '1', // Max view count of shortener
-    'cpm' => '21.00', // CPM of Shortener
-    'referral' => 'https://shorti.io', // Your Referral link
-    'status' => 'N' // Should be Y unless you dont want it to show in list then put N
-);
+
+function get_short_link($url, $apikey) {
+    // Shorten URL with first service
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://shorti.io/api?api={$apikey}&url={$url}");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    $data = json_decode($result, true);
+
+    if ($data['status'] == 'success') {
+        $shorturl = $data['shortenedUrl'];
+        $apiid = $data['apiId'];
+
+        // Shorten URL with second service
+        $ch2 = curl_init();
+        curl_setopt($ch2, CURLOPT_URL, "https://usalink.io/api?api={$apikey}&url={$shorturl}");
+        curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
+        $result2 = curl_exec($ch2);
+        curl_close($ch2);
+
+        $data2 = json_decode($result2, true);
+
+        if ($data2['status'] == 'success') {
+            $shorturl2 = $data2['shortenedUrl'];
+            return $shorturl2;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
 
 
